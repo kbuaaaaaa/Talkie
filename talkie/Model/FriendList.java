42aa7ed87801b786c7fb649de1b27c30c2da12f3
Karnbongkot Boonriong <karnbongkot@gmail.com>
2021-01-20 13:27:11 +0700

    Create FriendList.java

---
+++ talkie/Model/FriendList.java
@@ -0,0 +1,20 @@
+package com.example.talkie.Model;
+
+public class FriendList {
+    private String id;
+
+    public FriendList(String id) {
+        this.id = id;
+    }
+
+    public FriendList() {
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+}

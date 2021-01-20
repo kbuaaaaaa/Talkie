b2376f90512835b83d1056159473f268232d17aa
Karnbongkot Boonriong <karnbongkot@gmail.com>
2021-01-20 13:27:13 +0700

    Create Chatlist.java

---
+++ talkie/Model/Chatlist.java
@@ -0,0 +1,20 @@
+package com.example.talkie.Model;
+
+public class Chatlist {
+    private String id;
+
+    public Chatlist(String id) {
+        this.id = id;
+    }
+
+    public Chatlist() {
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
